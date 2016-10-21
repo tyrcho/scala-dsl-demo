9674e6b01056d926ade1a4a9909744d005329b94
[a518291] Michel Daviot <michel.daviot@atos.net>
2016-10-21 17:39:35 +0200

    sample which starts a HTTP server

---
+++ src/main/scala/StartServer.scala
@@ -0,0 +1,37 @@
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import org.glassfish.jersey.process.Inflector
+import org.glassfish.jersey.server.ResourceConfig
+import org.glassfish.jersey.server.model.Resource
+
+import javax.ws.rs.container.ContainerRequestContext
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
+import org.glassfish.grizzly.http.server.HttpServer
+
+object StartServer extends App {
+  // test at http://localhost:8081/path/test/hello?arg=aze&arg=rrr
+  createServer()
+
+  def createServer(scheme: String = "http",
+                   host: String = "0.0.0.0",
+                   port: Int = 8081,
+                   path: String = "/path"): HttpServer = {
+
+    val resourceBuilder = Resource.builder("/test")
+
+    resourceBuilder.addChildResource("/hello")
+      .addMethod("GET")
+      .handledBy(new Inflector[ContainerRequestContext, Response] {
+        def apply(context: ContainerRequestContext) = {
+          val args = context.getUriInfo.getQueryParameters.get("arg")
+          Response.ok(s"Hello $args").build
+        }
+      })
+
+    val resourceConfig = (new ResourceConfig).registerResources(resourceBuilder.build)
+
+    val uri = UriBuilder.fromPath(path).host(host).port(port).scheme(scheme).build()
+    GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig, true)
+  }
+
+}
